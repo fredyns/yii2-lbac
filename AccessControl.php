1b3b2f7f4ad028ef1819478da299d30ce389dc2d
fredy <fredy@fredy-Lenovo-B490>
2016-08-17 14:58:16 +0700

    basic action allowance

--- AccessControl.php
+++ AccessControl.php
@@ -90,4 +90,74 @@ class AccessControl extends \fredyns\lbac\BaseAccessControl
         ];
     }
 
+    public function allowIndex()
+    {
+        return true;
+    }
+
+    public function allowCreate()
+    {
+        return true;
+    }
+
+    public function allowView()
+    {
+        // prerequisites
+        if (empty($this->model) OR ( $this->model instanceof yii\db\ActiveRecord) == FALSE)
+        {
+            $this->addError('view', "Model is not an ActiveRecord.");
+
+            return false;
+        }
+
+        // blacklist
+        if ($this->model->isNewRecord)
+        {
+            $this->setError('view', "Cann't view unsaved Data.");
+        }
+
+        // conclusion
+        return ($this->isError('view') == FALSE);
+    }
+
+    public function allowUpdate()
+    {
+        // prerequisites
+        if (empty($this->model) OR ( $this->model instanceof yii\db\ActiveRecord) == FALSE)
+        {
+            $this->addError('update', "Model is not an ActiveRecord.");
+
+            return false;
+        }
+
+        // blacklist
+        if ($this->model->isNewRecord)
+        {
+            $this->setError('update', "Cann't view unsaved Data.");
+        }
+
+        // conclusion
+        return ($this->isError('update') == FALSE);
+    }
+
+    public function allowDelete()
+    {
+        // prerequisites
+        if (empty($this->model) OR ( $this->model instanceof yii\db\ActiveRecord) == FALSE)
+        {
+            $this->addError('delete', "Model is not an ActiveRecord.");
+
+            return false;
+        }
+
+        // blacklist
+        if ($this->model->isNewRecord)
+        {
+            $this->setError('delete', "Cann't view unsaved Data.");
+        }
+
+        // conclusion
+        return ($this->isError('delete') == FALSE);
+    }
+
 }
